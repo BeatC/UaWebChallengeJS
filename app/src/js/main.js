46f4a450791bc963f65ad67b5319fb7e5eef53a2
TheBeatCoder <shupuercha@gmail.com>
2015-03-23 22:25:04 +0200

    Set up frontend environment and almost finished page for testing developed data structure

---
+++ app/src/js/main.js
@@ -0,0 +1,52 @@
+(function () {
+	var startTestBtn = $('.btn-starttest'),
+		greetingsScreen = $('.greeting-section'),
+		s = Snap('.progress-graph'),
+		outerCircle,
+		innerCircle,
+		progressPath;
+
+	startTestBtn.click(function () {
+		greetingsScreen.animate({
+			opacity: 0
+		}, 300, 'swing', function () {
+			greetingsScreen.css({
+				display: 'none',
+				opacity: 1
+			});
+		});
+	});	
+
+	outerCircle = s.circle(75, 75, 74);
+	innerCircle = s.circle(75, 75, 60);
+	outerCircle.addClass('circle-outer');
+	innerCircle.addClass('circle-inner');
+
+	//progressPath = s.circle(75, 75, 72);
+
+	//progressPath.addClass('path-progress');
+})();
+
+
+// List of questions
+(function () {
+
+	var QuestionNode = Graph.Node.extend({
+		data: {
+			question: "",
+			answer: true
+		}
+	});
+
+	var root = new QuestionNode({
+		data: {
+			question: "",
+			answer: true
+		}
+	});
+
+		
+
+	root.AddChild();
+
+})();
